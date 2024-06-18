6fdcd2657a7162c6d33c0e2b8439b1e4e0667d56
Jonathan Torres <jnnnt.95@gmail.com>
2024-06-18 17:51:06 -0500

    test push

--- src/test/java/com/nniett/Class1Test.java
+++ src/test/java/com/nniett/Class1Test.java
@@ -8,7 +8,13 @@ public class Class1Test {
 
     @Test
     public void this_works() {
+        System.out.println("This worked!");
+    }
 
+    @Test
+    public void this_should_not_work() {
+        System.out.println("This did not work");
+        assert false;
     }
 
 }
